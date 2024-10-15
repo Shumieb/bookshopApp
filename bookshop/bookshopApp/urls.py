8e6926e6bb7fa7337221699a99c161cfbd511a48
Shumieb <shumiebc@gmail.com>
2024-10-15 02:47:01 +0100

    fixed names on urls file

--- bookshop/bookshopApp/urls.py
+++ bookshop/bookshopApp/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("all-books/", BooksPageView.as_view(), name="books"),
-    path("add-book/", AddBookPageView.as_view(), name="about"),
-    path("edit-book/", EditBookPageView.as_view(), name="about"),
+    path("add-book/", AddBookPageView.as_view(), name="addBook"),
+    path("edit-book/", EditBookPageView.as_view(), name="editBook"),
     path("about/", AboutPageView.as_view(), name="about"),
 ]
